80c68d1e58cd9597991841feb730afc3848c663e
AnimeHacker <jhankandry1899@gmail.com>
2022-06-29 21:19:42 -0500

    Creacion de componentes y modulos con rutas. 1- creacion de componente auth 2- creacion de componente favorites 3- creacion componente tracks 4- creacion componente home 5- creacion componente tracks
    Se crea router outlet en home routin para componentes de tracks, favorites y history

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,23 @@
+import { HomePageComponent } from ' @modules/home/pages/home-page/home-page.component';
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-  path: '',
-  loadChildren:() => import('./modules/home/home.module').then(m => m.HomeModule)
+    path: 'auth',
+    loadChildren: () =>
+      import('./modules/auth/auth.module').then(m => m.AuthModule),
+  },
+  {
+    path: '',
+    component: HomePageComponent,
+    loadChildren: () =>
+      import('./modules/home/home.module').then(m => m.HomeModule)
   }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
